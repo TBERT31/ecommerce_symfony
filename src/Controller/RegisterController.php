90ce6677e8bf6a4bd74f93c7611ee07c35fb1de0
Thomas Berteau <berteau.thomas@hotmail.fr>
2023-11-23 06:36:13 +0100

    login logout in course of build

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,13 +4,24 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface; // Importez la nouvelle interface
 
 class RegisterController extends AbstractController
 {
+    private $entityManager;
+    private $passwordHasher;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
+    }
+
     #[Route('/inscription', name: 'app_register')]
     public function index(Request $request): Response
     {
@@ -19,14 +30,18 @@ class RegisterController extends AbstractController
 
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid() ){
+        if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
 
-            dd($user);
+            $hashedPassword = $this->passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hashedPassword);
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
         }
 
         return $this->render('register/index.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
         ]);
     }
 }
