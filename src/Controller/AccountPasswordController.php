554d8f24de3f60a01fec1b42439aae474ecf0637
Thomas Berteau <berteau.thomas@hotmail.fr>
2023-11-23 16:25:37 +0100

    changement de mot de passe Okay

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -6,14 +6,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ChangePasswordType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AccountPasswordController extends AbstractController
 {
+    private $entityManager;
+    private $passwordHasher;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
+    }
+    
     #[Route('/compte/modifier-mot-de-passe', name: 'app_account_password')]
-    public function index(): Response
+    public function index(Request $request): Response
     {   
         $user = $this->getUser();
         $form = $this->createForm(ChangePasswordType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $old_password = $form->get('old_password')->getData();
+            
+            if ($this->passwordHasher->isPasswordValid($user, $old_password)) {
+                $new_password = $form->get('new_password')->getData();
+                $hashedPassword = $this->passwordHasher->hashPassword($user, $new_password);
+                
+                $user->setPassword($hashedPassword);
+
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+            } else {
+                // Le mot de passe actuel n'est pas valide, vous pouvez gérer cela en ajoutant une erreur au formulaire par exemple
+                $form->get('old_password')->addError(new \Symfony\Component\Form\FormError('Mot de passe incorrect.'));
+            }
+        }
 
         return $this->render('account/password.html.twig', [
             'form' => $form->createView(),
